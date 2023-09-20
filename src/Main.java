1d2728c20c081f1e70c3932ae10132e6c83e7413
romanurbanovic <romas.cpservisas@gmal.com>
2023-09-20 11:45:42 +0300

    FUNCIJOS 1 UZD

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//      1 uzduotis
+//      Sukurkite Funkciją kuri priima du int tipo kintamuosius. Juos susumuoja ir atspausdina.
+
+        //=)
+        int a = 7;
+        int b = 4;
+        duSkaic(a, b);
+
     }
+
+    public static void duSkaic(int c, int d) {
+        int sum = c + d;
+        System.out.println(sum);
+    }
+
 }
